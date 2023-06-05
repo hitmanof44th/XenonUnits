cc80bb2ad23fe4731e6dc61113a842b8637fbaa5
jt.boron <JT.BORON@GMAIL.COM>
2023-06-04 21:16:33 -0400

    added some start items

--- XenonUnits/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ XenonUnits/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace XenonUnits {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XenonUnits;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XenonUnits;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
